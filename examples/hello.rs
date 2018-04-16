17162ceec332928d0528f0a731b0624fab113633
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-04-16 18:03:50 +0200

    Add simple query and actually start Gotham
    This still does not work, probably because bb8 and Gotham are running on
    different event loops.
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,6 +1,8 @@
 extern crate bb8;
 extern crate bb8_postgres;
+extern crate env_logger;
 extern crate futures;
+extern crate futures_state_stream;
 extern crate gotham;
 extern crate gotham_middleware_bb8;
 extern crate hyper;
@@ -9,11 +11,12 @@ extern crate tokio_core;
 extern crate tokio_postgres;
 
 use std::error::Error;
-use std::io;
+use std::thread;
 
 use bb8::Builder as Bb8Builder;
 use bb8_postgres::PostgresConnectionManager;
 use futures::Future;
+use futures_state_stream::StateStream;
 use gotham::handler::HandlerFuture;
 use gotham::http::response::create_response;
 use gotham::pipeline::new_pipeline;
@@ -21,12 +24,25 @@ use gotham::pipeline::single::single_pipeline;
 use gotham::router::Router;
 use gotham::router::builder::*;
 use gotham::state::{FromState, State};
-use gotham_middleware_bb8::Bb8Middleware;
+use gotham_middleware_bb8::{Bb8Middleware, Bb8MiddlewareData};
 use hyper::StatusCode;
 use tokio_core::reactor::{Core, Remote};
 
 pub fn say_hello(state: State) -> Box<HandlerFuture> {
-    let f = { futures::future::ok::<String, io::Error>("hello".to_owned()) }.then(|res| {
+    let f = {
+        let pool = Bb8MiddlewareData::<PostgresConnectionManager>::borrow_from(&state).pool();
+
+        pool.run(|connection| {
+            connection
+                .prepare("SELECT 1")
+                .and_then(|(select, connection)| {
+                    connection.query(&select, &[]).for_each(|row| {
+                        println!("result: {}", row.get::<i32, usize>(0 as usize));
+                    })
+                })
+                .and_then(|connection| Ok(("hello".to_owned(), connection)))
+        })
+    }.then(|res| {
         let text = match res {
             Ok(text) => text,
             Err(err) => format!("Error: {}", err.description()),
@@ -45,7 +61,10 @@ pub fn say_hello(state: State) -> Box<HandlerFuture> {
 }
 
 fn router(remote: Remote) -> Box<Future<Item = Router, Error = ()>> {
-    let conn_mgr = PostgresConnectionManager::new("", || tokio_postgres::TlsMode::None).unwrap();
+    let conn_mgr = PostgresConnectionManager::new(
+        "postgresql://postgres:mysecretpassword@localhost:5432",
+        || tokio_postgres::TlsMode::None,
+    ).unwrap();
     let f = Bb8Builder::new()
         .build(conn_mgr, remote)
         .and_then(|pool| {
@@ -65,14 +84,21 @@ fn router(remote: Remote) -> Box<Future<Item = Router, Error = ()>> {
 }
 
 pub fn main() {
+    env_logger::init();
+
     let addr = "127.0.0.1:7878";
     let mut core = Core::new().unwrap();
 
     let remote = core.remote();
 
-    core.run(router(remote).and_then(|_| {
+    core.run(router(remote).and_then(|router| {
         println!("Listening for requests at http://{}", addr);
-        //gotham::start(addr)
+
+        thread::spawn(move || {
+            gotham::start(addr, router);
+        }).join()
+            .unwrap();
+
         Ok(())
     })).unwrap();
 }
